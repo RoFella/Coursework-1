37076ba7cbe97c5e349c5dbcb1b03e78bc3208f8
RoFella <den00571@gmail.com>
2022-11-02 20:08:25 +0000

    Completed Currency Conversion

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -5,8 +5,11 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(number1) {
+  return "$" + (number1 * 1.4).toFixed(2)
+}
 
+console.log(convertToBRL(25))
 /*
   CURRENCY CONVERSION
   ===================
@@ -15,7 +18,10 @@ function convertToUSD() {}
   They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
 */
 
-function convertToBRL() {}
+function convertToBRL(number2) {
+  let MoneyWithPerc = (number2 - (number2 / 100))
+  return  "R$" + (MoneyWithPerc * 5.7).toFixed(2)
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
